378b10cac9649d375878bbefa2b2c0cd08db00e8
zapodask <joaopedroemerim@gmail.com>
2023-10-24 14:45:35 -0300

    feat: config env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
+
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 15432,
-      username: 'postgres',
-      password: 'postgres@2022',
-      database: 'irmandadedocodigo',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: true,
     }),
--- src/auth/constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-  secret: '@nL3D9G&fut@yfyb%i%^2S!ufxo92oM6',
-};
