1d48d9994bdd1c8853548d57ef399cb0238443cf
zapodask <joaopedroemerim@gmail.com>
2023-10-31 23:04:55 -0300

    feat: mail service

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,21 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendPasswordResetLink(user: User, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      subject: 'Password Reset',
+      template: './forgotPassword',
+      context: {
+        url,
+      },
+    });
+  }
+}
